f711bfd52b234c4874ab7a64109d057629b7bd39
lamine <lamine@lamine.git>
2018-10-21 17:37:18 +0200

    [LAI] FEATURE : modification du service qui retourne la liste des quizzes

--- src/main/java/com/lamine/animebasho/services/interfaces/QuizService.java
+++ src/main/java/com/lamine/animebasho/services/interfaces/QuizService.java
@@ -1,15 +1,24 @@
 package com.lamine.animebasho.services.interfaces;
 
+import com.lamine.animebasho.entities.Quiz;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Set;
+
 @Api(value = "Quiz", description = "Service de gestion des quizzes", tags = { "Quiz" })
 @RequestMapping(value = "/api")
 public interface QuizService {
 
+    /**
+     * Méthode qui retourne la liste des quizzes présents en base,
+     * avec les questions et propositions associées.
+     *
+     * @return
+     */
     @ApiOperation(httpMethod = "GET", value = "Retourne la liste des quizzes")
     @GetMapping(value = "/quizzes")
-    public String getQuizzes();
+    public Set<Quiz> getQuizzes();
 }
