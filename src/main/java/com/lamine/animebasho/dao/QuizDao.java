73bc12d02cd8fa59b97ae3c511ba89e77d522625
lamine <lamine@lamine.git>
2018-10-21 17:26:19 +0200

    [LAI] FEATURE : récupération des quizzes complets via une requête JPQL

--- src/main/java/com/lamine/animebasho/dao/QuizDao.java
+++ src/main/java/com/lamine/animebasho/dao/QuizDao.java
@@ -1,7 +1,7 @@
 package com.lamine.animebasho.dao;
 
 import com.lamine.animebasho.entities.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +9,6 @@ import java.util.Set;
 
 @Repository
 public interface QuizDao extends CrudRepository<Quiz, Long> {
-    Set<Quiz> findAll();
+    @Query("select q from Quiz q join fetch q.questions qu join fetch qu.propositions p")
+    Set<Quiz> findQuizzes();
 }
