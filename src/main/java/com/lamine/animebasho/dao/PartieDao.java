861e25a76672888ef59db3d155df4e2330a93d85
lamine <lamine@lamine.git>
2018-10-21 23:12:30 +0200

    [LAI] FEATURE : création de la query qui récupère la partie

--- src/main/java/com/lamine/animebasho/dao/PartieDao.java
+++ src/main/java/com/lamine/animebasho/dao/PartieDao.java
@@ -1,9 +1,13 @@
 package com.lamine.animebasho.dao;
 
 import com.lamine.animebasho.entities.Partie;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PartieDao extends CrudRepository<Partie, Long> {
+    @Query("select p from Partie p join fetch p.reponses rep where p.id = :id")
+    Partie findPartieById(@Param("id") Long id);
 }
