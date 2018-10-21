6cb90a1b993b90e1ccbcc60423168fb2992a8fc7
lamine <lamine@lamine.git>
2018-10-21 20:55:05 +0200

    [LAI] FEATURE : préparation de la réponse HTTP suivant la création de la partie

--- src/main/java/com/lamine/animebasho/handler/PartieHandler.java
+++ src/main/java/com/lamine/animebasho/handler/PartieHandler.java
@@ -1,10 +1,15 @@
 package com.lamine.animebasho.handler;
 
 import com.lamine.animebasho.dao.PartieDao;
+import com.lamine.animebasho.dto.PartieDto;
 import com.lamine.animebasho.entities.Partie;
 import com.lamine.animebasho.exceptions.PartieNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @Component
 public class PartieHandler {
@@ -16,5 +21,25 @@ public class PartieHandler {
         return partieDao
                 .findById(partieId)
                 .orElseThrow(() -> new PartieNotFoundException("La partie "+ partieId + " n'existe pas !"));
+    }
+
+    public ResponseEntity enregistrerPartie(PartieDto partieDto) {
+        Partie partie = buildPartie(partieDto);
+
+        Long partieId = partieDao.save(partie).getId();
+        URI location = buildUrlResource(partieId);
+        return ResponseEntity.created(location).build();
+    }
+
+    private Partie buildPartie(PartieDto partieDto) {
+        return null;
+    }
+
+    private URI buildUrlResource(Long partieId) {
+        return ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{partieId}")
+                    .buildAndExpand(partieId)
+                    .toUri();
     }
 }
