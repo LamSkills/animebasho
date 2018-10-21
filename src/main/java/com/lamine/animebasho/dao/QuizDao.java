eaa5b7ebd557341f463ecc57560480e867d8d325
lamine <lamine@lamine.git>
2018-10-21 16:36:33 +0200

    [LAI] FEATURE : utilisation de CrudRepository pour manipuler les Set

--- src/main/java/com/lamine/animebasho/dao/QuizDao.java
+++ src/main/java/com/lamine/animebasho/dao/QuizDao.java
@@ -2,8 +2,12 @@ package com.lamine.animebasho.dao;
 
 import com.lamine.animebasho.entities.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
-public interface QuizDao extends JpaRepository<Quiz, Long> {
+public interface QuizDao extends CrudRepository<Quiz, Long> {
+    Set<Quiz> findAll();
 }
