1986b958f8263a1aa5a0589f537eb769400df95b
lamine <lamine@lamine.git>
2018-10-21 23:15:48 +0200

    [LAI] FEATURE : création d'une classe d'exception générique pour le code HTTP 404

--- src/main/java/com/lamine/animebasho/exceptions/EntityNotFoundException.java
+++ src/main/java/com/lamine/animebasho/exceptions/EntityNotFoundException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class PartieNotFoundException extends RuntimeException {
+public class EntityNotFoundException extends RuntimeException {
 
-    public PartieNotFoundException(String message) {
+    public EntityNotFoundException(String message) {
         super(message);
     }
 }
