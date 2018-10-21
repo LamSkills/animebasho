db8a0dd8c41020c7192a8439f644c0dbbe8ff447
lamine <lamine@lamine.git>
2018-10-21 19:04:36 +0200

    [LAI] FIX : correction de la stratégie de génération des id par Hibernate

--- src/main/java/com/lamine/animebasho/entities/Proposition.java
+++ src/main/java/com/lamine/animebasho/entities/Proposition.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 public class Proposition {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "label")
