66413a05b730d452ebea505ebe45e6dcbea576c9
lamine <lamine@lamine.git>
2018-10-21 19:16:23 +0200

    [LAI] FIX : ajout des méthodes equals/hashCode

--- src/main/java/com/lamine/animebasho/entities/Proposition.java
+++ src/main/java/com/lamine/animebasho/entities/Proposition.java
@@ -3,6 +3,7 @@ package com.lamine.animebasho.entities;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class Proposition {
@@ -51,6 +52,21 @@ public class Proposition {
 
     public void setQuestion(Question question) {
         this.question = question;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Proposition that = (Proposition) o;
+        return isCorrect == that.isCorrect &&
+                Objects.equals(id, that.id) &&
+                Objects.equals(label, that.label);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, label, isCorrect);
     }
 
     @Override
