8fcfca9a49fd0885820540cc14be0eea2cbc23b1
lamine <lamine@lamine.git>
2018-10-28 20:50:47 +0100

    [LAI] SONAR : suppression d'un import inutilisé

--- src/main/java/com/lamine/animebasho/handler/PartieHandler.java
+++ src/main/java/com/lamine/animebasho/handler/PartieHandler.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
